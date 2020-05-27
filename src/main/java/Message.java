561e0c22e7523de64d272a7e8827972cbabeb7a8
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-05-27 20:01:04 +0300

    Remove extra files from the tutorial here
    https://www.baeldung.com/java-websockets

--- src/main/java/Message.java
+++
@@ -1,7 +0,0 @@
-package websocket.demo;
-
-public class Message {
-  private String coordX;
-  private String coordY;
-  private String coordZ;
-}
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,90 +2,12 @@ package websocket.demo;
 
 import javax.websocket.server.*;
 import javax.websocket.*;
-
-import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.Set;
-import java.util.HashMap;
-import java.io.IOException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.*;
 
-@ServerEndpoint(value="/abc")
+@ServerEndpoint(value="/endpoint")
 public class Server {
   @OnOpen
   public void onOpen() throws IOException {
     System.out.println("The test did pass!");
   }
 }
-
-/*
-@ServerEndpoint(value="/chat/{username}",
-                decoders = MessageDecoder.class,
-                encoders = MessageEncoder.class)
-
-public class Server {
-
-  private static HashMap<String, String> users = new HashMap<>();
-  private static Set<Server> chatEndpoints = new CopyOnWriteArraySet<>();
-
-  private Session session;
-
-  @OnOpen
-  public void onOpen(Session session,
-                     @PathParam("username") String username)
-      throws IOException {
-
-    this.session = session;
-    chatEndpoints.add(this);
-    users.put(session.getId(), username);
-
-    Message message = new Message();
-    message.setFrom(username);
-    message.setContent("Connected!");
-    try {
-      broadcast(message);
-    } catch (Exception e) {}
-  }
-
-  @OnMessage
-  public void onMessage(Session session, Message message)
-      throws IOException {
-
-    message.setFrom(users.get(session.getId()));
-    try {
-      broadcast(message);
-    } catch (Exception e) {}
-  }
-
-  @OnClose
-  public void onClose(Session session) throws IOException {
-
-    chatEndpoints.remove(this);
-    Message message = new Message();
-    message.setFrom(users.get(session.getId()));
-    message.setContent("Disconnected!");
-    try {
-      broadcast(message);
-    } catch (Exception e) {}
-  }
-
-  @OnError
-  public void onError(Session session, Throwable throwable) {}
-
-  private static void broadcast(Message message)
-      throws IOException, EncodeException {
-
-    chatEndpoints.forEach(endpoint -> {
-        synchronized (endpoint) {
-          try {
-            endpoint.session.getBasicRemote().
-                sendObject(message);
-          } catch (IOException | EncodeException e) {
-            e.printStackTrace();
-          }
-        }
-      });
-  }
-}
-*/
