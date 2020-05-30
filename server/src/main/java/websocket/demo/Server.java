8bb114ef49c17a91047a6fb650d44b8b0cf6182c
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-05-30 15:35:14 +0300

    Improve error handling in the server.

--- server/src/main/java/websocket/demo/Server.java
+++ server/src/main/java/websocket/demo/Server.java
@@ -3,6 +3,7 @@ package websocket.demo;
 import javax.websocket.server.*;
 import javax.websocket.*;
 import java.io.*;
+import java.lang.*;
 
 @ServerEndpoint(value="/entry")
 public class Server {
@@ -11,13 +12,23 @@ public class Server {
   public void onMessage(String data, Session session) {
     try {
       session.getBasicRemote().sendText("1|2|3");
+      System.out.println("Sent the message");
     } catch (IOException e) {
       System.out.println("Unable to send message...");
     }
   }
 
+  @OnError
+  public void onError(Throwable err) {
+    err.printStackTrace();
+  }
+
   @OnOpen
-  public void onOpen() throws IOException {
-    System.out.println("The client has connected!");
+  public void onOpen() {
+    try {
+      System.out.println("The client has connected!");
+    } catch (Exception e) {
+      System.out.println("Unable to open...");
+    }
   }
 }
